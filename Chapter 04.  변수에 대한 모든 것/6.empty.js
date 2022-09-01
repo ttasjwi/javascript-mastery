3eafd8773b028538c1009d062a784ddbca0d2b22
ttasjwi <ttasjwi920@gmail.com>
2022-09-01 22:46:14 +0900

    4.7 null, undefined

---
+++ Chapter 04.  변수에 대한 모든 것/6.empty.js
@@ -0,0 +1,12 @@
+// null(값이 없다는걸 확실히 명시), undefined(값이 정흐ㅐ지지 않음)
+let variable;
+console.log(variable);
+
+variable = null;
+console.log(variable);
+
+let activeItem; // 아직 활성화된 아이템이 있는지 없는지 모르는 상태!
+activeItem =  null; // 활성화된 아이템이 없는 상태
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
