bc2eec3d26918d1ece1896cb1f7791f6c2bf0e76
ttasjwi <ttasjwi920@gmail.com>
2022-09-23 15:38:35 +0900

    5.5 할당 연산자

---
+++ Chapter 05. 연산자/4.assignment.js
@@ -0,0 +1,17 @@
+// 할당 연산자 (Assignment operators)
+let a = 1;
+a = a + 2; // (cf) 할당문은 그 자체로 표현식인 문이다.
+console.log(a);
+
+a += 2; // a= a+2 축약
+console.log(a); // 5
+
+a -= 2;
+console.log(a); // 3
+
+a *= 2;
+console.log(a); // 6
+
+a /= 2;
+a %= 2;
+a **= 2;
