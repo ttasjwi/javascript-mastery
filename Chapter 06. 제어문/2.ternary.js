11b0b23b82f57241d5e5150f420b6524b8b72927
ttasjwi <ttasjwi920@gmail.com>
2022-09-23 16:35:07 +0900

    6.3 삼항 연산자

---
+++ Chapter 06. 제어문/2.ternary.js
@@ -0,0 +1,13 @@
+// 삼항 조건 연산자 Ternary Operator
+// 조건식 ? 참인 경우 : 거짓인 경우
+
+let fruit = 'apple';
+
+fruit === 'apple'
+    ? console.log('사과!!!')
+    : console.log('아니오 아니오');
+
+let result = fruit === 'apple'
+    ? '사과!!!!'
+    : '아니오 아니오';
+console.log(result)
