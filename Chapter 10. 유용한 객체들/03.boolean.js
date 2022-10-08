0764ce9a90ff14e6fb273761ef877128ffeca802
ttasjwi <ttasjwi920@gmail.com>
2022-10-08 23:34:34 +0900

    10.4 Boolean과 함수들

---
+++ Chapter 10. 유용한 객체들/03.boolean.js
@@ -0,0 +1,30 @@
+
+//Boolean
+const isTrue = new Boolean(true);
+console.log(isTrue.valueOf());
+
+/**
+ * Falshy
+ * 0
+ * -0
+ * null
+ * NaN
+ * undefined
+ * ''
+ */
+
+/**
+ * Truthy
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+ *
+ */
+
+/**
+ * Boolean 참고자료
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+ */
