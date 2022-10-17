6162379dacbd42757ddf656f43bc9b0da5554e9f
ttasjwi <ttasjwi920@gmail.com>
2022-10-17 19:19:01 +0900

    15.02 에러 처리

---
+++ Chapter 15. 주석, 에러 처리, 모듈/15.02 ErrorHandling(에러 처리)/15.02.error-handling.js
@@ -0,0 +1,28 @@
+function readFile(path) {
+    if (path === 'ex') {
+        throw new Error("파일 경로를 찾을 수 없음");
+    }
+    return '파일의 내용';
+}
+
+function processFile(path) {
+    let content;
+    try {
+        content = readFile(path);
+    } catch (error) { // 예외를 잡아 처리
+        console.log(error.name);
+        console.log(error.message);
+        console.log(error.stack);
+        content = '기본내용';
+    } finally {
+        console.log('성공하든 실패하든 실패하든 마지막으로 리소스 정리!');
+    }
+    const result = 'hi ' + content;
+    return result;
+}
+
+const result = processFile('경로');
+console.log(result);
+
+const result2 = processFile('ex');
+console.log(result2);
