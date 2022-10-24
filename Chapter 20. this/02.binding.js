7a341c743e06060ab8de3e078189e41820133a5f
ttasjwi <ttasjwi920@gmail.com>
2022-10-24 19:19:43 +0900

    20.3 this - 동적 바인딩
    - javascript의 this는 호출하는 사람(caller)에 의해 누구를 가리키는 지 동적으로 결정됨

---
+++ Chapter 20. this/02.binding.js
@@ -0,0 +1,40 @@
+/**
+ * this 바인딩
+ *
+ * java, C#, C++ 등의 대부분의 객체지향 프로그래밍 언어에서는
+ * this는 항상 자신의 인스턴스 자체를 가리킴
+ * 정적으로 인스턴스가 만들어지는 시점에 this가 결정됨. 변경될 수 없음.
+ *
+ * 하지만, 자바스크립트에서는 누가 호출하냐에 따라서, this가 달라질 수 있음.
+ * 즉, this는 호출하는 사람(caller)에 의해 동적으로 결정됨!
+ */
+
+
+function Cat(name) {
+    this.name = name;
+    this.printName = function()  {
+        console.log(`고양이의 이름을 출력한다옹: ${this.name}`);
+    };
+}
+
+function Dog(name) {
+    this.name = name;
+    this.printName = function()  {
+        console.log(`강아지의 이름을 출력한다옹: ${this.name}`);
+    };
+}
+
+const cat = new Cat('냐옹');
+const dog = new Dog('멍멍');
+cat.printName();
+dog.printName();
+
+dog.printName = cat.printName;
+dog.printName(); // 고양이의 이름을 출력한다옹: 멍멍 (dog 인스턴스가 호출했으므로, dog의 프로퍼티 this.name이 바인딩 됨)
+
+function printOnMonitor(printName) {
+    console.log('모니터를 준비하고, 전달된 콜백을 실행!')
+    printName();
+}
+
+printOnMonitor(cat.printName); // 호출한 쪽 기준에 맞게, 동적으로 this가 호출됨 => undefined
