3dc416044dbb826c7a1e553d9794b7d66b4c7ed7
ttasjwi <ttasjwi920@gmail.com>
2022-10-24 14:47:03 +0900

    18.5 프로토타입 레벨 함수

---
+++ Chapter 18. 프로토타입/03.prototype.js
@@ -0,0 +1,38 @@
+// const dog1 = {name: '뭉치', emoji: '🐶'};
+// const dog2 = {name: '코코', emoji: '🐩'};
+
+function Dog(name, emoji) {
+    this.name = name;
+    this.emoji = emoji;
+
+    // 인스턴스 레벨의 함수 => 메모리 낭비
+    // this.printName = () => {
+    //     console.log(`${this.name} ${this.emoji}`);
+    // }
+}
+
+// 프로토타입에 printName 함수 등록 => 인스턴스 공통 메서드
+Dog.prototype.printName = function() {
+    console.log(`${this.name} ${this.emoji}`);
+};
+
+const dog1 = new Dog('뭉치', '🐶');
+const dog2 = new Dog('코코', '🐩');
+console.log(dog1, dog2);
+dog1.printName();
+dog2.printName();
+
+// 오버라이딩
+// 인스턴스 레벨에서(자식) 동일한 이름으로 함수를 재정의하면(오버라이딩)
+// 프로토타입 레벨의(부모) 함수의 프로퍼티는 가려진다.(섀도잉 된다.)
+dog1.printName = function() {
+    console.log('안녕');
+}
+dog1.printName();
+
+// 정적(static) 레벨
+Dog.hello = () => {
+    console.log('Hello');
+};
+Dog.hello();
+Dog.MAX_AGE = 20;
